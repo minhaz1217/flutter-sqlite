c3fb96dcb6cf67e37d4f6eb383403aeb162fa2c8
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2019-04-30 12:58:45 +0600

    created the model of the table

---
+++ lib/ClientModel.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+Client clientFromJson(String str) {
+  final jsonData = json.decode(str);
+  return Client.fromMap(jsonData);
+}
+
+String clientToJson(Client data){
+  final dyn = data.toMap();
+  return json.encode(dyn);
+}
+
+class Client{
+  int id;
+  String firstName;
+  String lastName;
+  bool blocked;
+  Client({
+    this.id,
+    this.firstName,
+    this.lastName,
+    this.blocked,
+});
+  factory Client.fromMap(Map<String, dynamic> json) => new Client(
+    id: json["id"],
+    firstName: json["firstName"],
+    lastName: json["lastName"],
+    blocked: json["blocked"],
+  );
+
+  Map<String, dynamic> toMap() => {
+    "id" : id,
+    "firstName" : firstName,
+    "lastName" : lastName,
+    "blocked" : blocked,
+  };
+}
+
